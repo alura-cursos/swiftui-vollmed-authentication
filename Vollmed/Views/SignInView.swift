541aeaef1f16b1e67fa79278a347ac4dad7cb89e
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-09-18 21:27:45 -0300

    aula 1.4 - construindo a tela de cadastro

--- Vollmed/Views/SignInView.swift
+++ Vollmed/Views/SignInView.swift
@@ -65,6 +65,7 @@ struct SignInView: View {
             }
         }
         .padding()
+        .navigationBarBackButtonHidden()
     }
 }
 
--- Vollmed/Views/SignUpView.swift
+++ Vollmed/Views/SignUpView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @State private var name: String = ""
+    @State private var email: String = ""
+    @State private var cpf: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var healthPlan: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack(alignment: .leading, spacing: 16.0) {
@@ -26,6 +34,77 @@ struct SignUpView: View {
                     .font(.title3)
                     .foregroundStyle(.gray)
                     .padding(.bottom)
+                
+                Text("Nome")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                TextField("Insira seu nome completo", text: $name)
+                    .padding(14)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                    .autocorrectionDisabled()
+                
+                Text("Email")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                TextField("Insira seu email", text: $email)
+                    .padding(14)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                    .autocorrectionDisabled()
+                    .keyboardType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+                
+                Text("CPF")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                TextField("Insira seu CPF", text: $cpf)
+                    .padding(14)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                    .keyboardType(.numberPad)
+                
+                Text("Telefone")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                TextField("Insira seu telefone", text: $phoneNumber)
+                    .padding(14)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                    .keyboardType(.numberPad)
+                
+                Text("Senha")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                SecureField("Insira sua senha", text: $password)
+                    .padding(14)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                
+                Button(action: {
+                    //
+                }, label: {
+                    ButtonView(text: "Cadastrar")
+                })
+                
+                NavigationLink {
+                    SignInView()
+                } label: {
+                    Text("Já possui uma conta? Faça o login!")
+                        .bold()
+                        .foregroundStyle(.accent)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
             }
         }
         .navigationBarBackButtonHidden()
